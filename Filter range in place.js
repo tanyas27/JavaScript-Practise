489092c779242bfed7bd386528a7811b66b17031
Tanya Singh <tanyacs27@gmail.com>
2020-01-14 10:17:47 +0530

    Removes all the values from the array which are not in sepcified range.

---
+++ Filter range in place.js
@@ -0,0 +1,19 @@
+/*filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b.
+ The test is: a ≤ arr[i] ≤ b. */
+ let arr = [5, 3, 8, 1];
+
+console.log(filterRangeInPlace(arr, 1, 4)); // removed the numbers except from 1 to 4
+
+alert( arr ); // [3, 1]
+function filterRangeInPlace(arr,a,b)
+{
+ for(let i =0; i< arr.length ; i++)
+  {
+    if(arr[i]>= a && arr[i] <= b){
+       
+    }
+    else{
+        arr.splice(i,1);
+    }
+  }
+}
