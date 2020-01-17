33ee90b5e8c2a08e4734004f4cf453df7db4982a
Tanya Singh <tanyacs27@gmail.com>
2020-01-17 21:10:43 +0530

    minor change

--- Store unread flags.js
+++ Store unread flags.js
@@ -17,3 +17,23 @@ P.S. When a message is removed from messages, it should disappear from your stru
 P.P.S. We shouldn’t modify message objects, add our properties to them. 
 As they are managed by someone else’s code, that may lead to bad consequences.
 */
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
+let readmessages = new WeakSet();
+// two messages have been read
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+// readMessages has 2 elements
+
+// ...let's read the first message again!
+readMessages.add(messages[0]);
+// readMessages still has 2 unique elements
+
+// answer: was the message[0] read?
+alert("Read message 0: " + readMessages.has(messages[0])); // true
+
+messages.shift();
+// now readMessages has 1 element (technically memory may be cleaned later)
