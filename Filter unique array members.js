09843fdb981650c52a227cede6aa36e25116314c
Tanya Singh <tanyacs27@gmail.com>
2020-01-17 12:16:37 +0530

    filter unique array members

---
+++ Filter unique array members.js
@@ -0,0 +1,24 @@
+/* Let arr be an array.
+
+Create a function unique(arr) that should return an array with unique items of arr. */
+function unique(arr) {
+    let set = new Set(arr);
+     return Array.from(set);
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  console.log( unique(strings) ); // Hare, Krishna, :-O
+
+  //**************OR************ Second Method without using set
+  function unique(arr){
+   let result = [];
+   for(let s of arr){
+       if(!result.includes(s)){
+         result.push(s);
+       }
+   }
+   return result;
+  }
