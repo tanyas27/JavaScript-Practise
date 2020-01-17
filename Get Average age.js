5ffa2cba3ca04b41f2b87eb3775d574cb382e3b5
Tanya Singh <tanyacs27@gmail.com>
2020-01-17 11:54:16 +0530

    Get average ages

---
+++ Get Average age.js
@@ -0,0 +1,13 @@
+/*Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+
+The formula for the average is (age1 + age2 + ... + ageN) / N.*/
+function getAverageAge(users){
+  return arr.reduce((sum,a)=> sum+a.age,0)/arr.length;
+}
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
